07ec768e3d37c847cdbe929a4d919aba887d3dd7
manana <manana@A13E21>
2024-04-19 12:14:43 +0200

    19/04

--- 22_SORT_ordenacion_listas/src/pruebas/PruebaLambda.java
+++ 22_SORT_ordenacion_listas/src/pruebas/PruebaLambda.java
@@ -7,14 +7,14 @@ public class PruebaLambda {
 	public static void main(String[] args) {
 		int a=2;
 		int b=a++;
-		Consumer<Integer> con=x->System.out.println(x+a);
+		//Consumer<Integer> con=x->System.out.println(x+a);
 		//Dentro de un lanbda no puede haber variables locales
 		
 		// En una expresión Lambda no se pueden usar variables locales
 		//
-		Consumer<Integer> con=x->System.out.println(x+b); //OK
+		Consumer<Integer> con2=x->System.out.println(x+b); //OK
 		
-		Consumer<Integer> con=x->System.out.println(x+b+d); //OK
+		Consumer<Integer> con3=x->System.out.println(x+b+s); //OK
 		s++;
 
 		
